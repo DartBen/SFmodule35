2774789e9c8c482402bf1b923838bbd45c87c1d0
DartBenSecond <ryabchikk@rambler.ru>
2023-05-15 22:00:11 +0300

    работает логин, логаут не работает хотя вроде бы все привязки к методу верные-брауезр не отпровляет запрос на logout.

--- SocialNetwork/SocialNetwork/ViewModels/Account/RegisterViewModel.cs
+++ SocialNetwork/SocialNetwork/ViewModels/Account/RegisterViewModel.cs
@@ -43,6 +43,6 @@ namespace SocialNetwork.ViewModels.Account
 
         [Required]
         [Display(Name = "Никнейм")]
-        public string Login { get; set; }
+        public string Login => EmailReg;
     }
 }
