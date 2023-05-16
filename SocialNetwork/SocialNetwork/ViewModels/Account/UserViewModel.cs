479055e6344f75d082dbc5d25eea5f1cbbd41fb1
DartBenSecond <ryabchikk@rambler.ru>
2023-05-16 21:33:01 +0300

    добавленно UserViewModel

---
+++ SocialNetwork/SocialNetwork/ViewModels/Account/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SocialNetwork.Data;
+
+namespace SocialNetwork.ViewModels.Account
+{
+    public class UserViewModel
+    {
+        public User User { get; set; }
+
+        public UserViewModel(User user)
+        {
+            User = user;
+        }
+    }
+}
