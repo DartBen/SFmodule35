f79bb6ceab0dfd22a17a155bdb11fb6a87bf0a83
DartBenSecond <ryabchikk@rambler.ru>
2023-05-20 14:00:06 +0300

    изменён редирект при логине

--- SocialNetwork/SocialNetwork/Controllers/HomeController.cs
+++ SocialNetwork/SocialNetwork/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Data.UnitOfWorks;
 using SocialNetwork.Models;
+using SocialNetwork.Models.Users;
 using SocialNetwork.ViewModels;
 using System.Diagnostics;
 
@@ -8,18 +12,33 @@ namespace SocialNetwork.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private IMapper _mapper;
+        private IUnitOfWork _unitOfWork;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<User> userManager, 
+            SignInManager<User> signInManager, IMapper mapper, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
         }
 
         [Route("")]
         [Route("[controller]/[action]")]
         public IActionResult Index()
         {
-            var result = new MainViewModel();
-            return View(result);
+            if (_signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("MyPage", "AccountManager");
+            }
+            else
+            {
+                return View(new MainViewModel());
+            }
         }
 
         [Route("[action]")]
